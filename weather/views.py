a3fc8495f199660482f89f9613d9c3736d11da71
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-06-18 13:55:36 -0600

    changing our supplier of the api

--- weather/views.py
+++ weather/views.py
@@ -7,55 +7,47 @@ import requests
 
 class weatherView(viewsets.ViewSet):  
             
-# --------------------End points to get the weather information either with the name of the place or with the latitud and the longitud--------------               
+# --------------------End points to get the weather information of the country information with putting the name of the country--------------               
         @action(detail=False, methods=['GET'], url_path='get_weather')
         def get_weather(self, request):
-            user_city = request.query_params.get('place_id', None)
+            user_city = request.query_params.get('q', None)
             
             if not user_city:
-                return Response({"Error:" "You're messing one information, example(place_id=INFO"}, status=status.HTTP_400_BAD_REQUEST)                 
-            url = f"https://www.meteosource.com/api/v1/free/point?place_id={user_city}&sections=current%2C%20hourly&timezone=auto&language=en&units=auto&key=lvetw31pfazam0dvgan3k4u9p5qu7u89wf17xgvb"
+                return Response({"Error:" "You're messing one information, example of what you need to put in the url at the end(?q=INFO) "}, status=status.HTTP_400_BAD_REQUEST)               
+             
+            url = f"http://api.weatherapi.com/v1/current.json?key=bdedcc2092b24167a9e164011251806&q={user_city}&aqi=no"
             response = requests.get(url)
             data = response.json()
             return Response(data)
         
-        @action(detail=False, methods=['GET'], url_path='get_weather2')
-        def get_weather2(self, request):
-            user_lat = request.query_params.get('lat', None)
-            user_lon = request.query_params.get('lon', None) 
-            
-            if not user_lat and user_lon:
-                return Response({"Error:" "You're messing one information, example(lat = number and lon = number"}, status=status.HTTP_400_BAD_REQUEST)                 
-            url = f"https://www.meteosource.com/api/v1/free/point?lat={user_lat}&lon={user_lon}&sections=current%2C%20hourly&timezone=auto&language=en&units=auto&key=lvetw31pfazam0dvgan3k4u9p5qu7u89wf17xgvb"
-            response = requests.get(url)
-            data = response.json()
-            return Response(data)
+       
         
-        
-        
-
-# --------------------End point to get a place putting the name or the zip code--------------                
+    
+# --------------------End point to get a place putting the name--------------                
         @action(detail=False, methods=['GET'], url_path='get_place')
         def get_place(self, request):
-            user_city = request.query_params.get('place_id', None)
+            user_city = request.query_params.get('q', None)
             if not user_city:
-                return Response({"Error:" "You're messing one information, example(place_id=INFO"}, status=status.HTTP_400_BAD_REQUEST)
-            url = f"https://www.meteosource.com/api/v1/free/find_places?text={user_city}&language=en&key=lvetw31pfazam0dvgan3k4u9p5qu7u89wf17xgvb"
+                   return Response({"Error: " "You're messing one information, example of what you need to put in the url at the end(?q=INFO) "}, status=status.HTTP_400_BAD_REQUEST)           
+            
+            url = f"http://api.weatherapi.com/v1/timezone.json?key=bdedcc2092b24167a9e164011251806&q={user_city}"
             response = requests.get(url)
             data = response.json()
             return Response(data)
+
+
 
 
 # --------------------End point for the neaarest place between two differents latitud and longitud--------------        
-        @action(detail=False, methods=['GET'], url_path='nearest_place')
-        def nearest_place(self, request):
-            user_lat = request.query_params.get('lat', None)
-            user_lon = request.query_params.get('lon', None) 
-            if not user_lat and user_lon:
-                return Response({"Error":"You're messing two information, example (lat = Numbers, lon = numbers)"}, status=status.HTTP_400_BAD_REQUEST)
-
-            url = f"https://www.meteosource.com/api/v1/free/nearest_place?lat={user_lat}&lon={user_lon}&language=en&key=lvetw31pfazam0dvgan3k4u9p5qu7u89wf17xgvb"
+        @action(detail=False, methods=['GET'], url_path='get_astronomy')
+        def get_astronomy(self, request):
+            user_city = request.query_params.get('q', None)
             
+            if not user_city:
+                  return Response({"Error:" "You're messing one information, example of what you need to put in the url at the end(?q=INFO) "}, status=status.HTTP_400_BAD_REQUEST)        
+            
+            url = f" http://api.weatherapi.com/v1/astronomy.json?key=bdedcc2092b24167a9e164011251806&q={user_city}&dt=2025-06-18 "
             response = requests.get(url)
             data = response.json()
             return Response(data)
+        
